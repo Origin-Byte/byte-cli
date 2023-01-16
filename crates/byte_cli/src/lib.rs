b75a6a5790e16d43f6cf657af82f670ccdb3ddd2
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-01-16 21:53:30 +0000

    Init Collection Command

--- crates/byte_cli/src/lib.rs
+++ crates/byte_cli/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod cli;
 pub mod consts;
+pub mod endpoints;
 pub mod prelude;
--- crates/byte_cli/src/prelude.rs
+++ crates/byte_cli/src/prelude.rs
@@ -1,2 +1,3 @@
 pub use crate::cli::{Cli, Commands};
 pub use crate::consts;
+pub use crate::endpoints::init_config;
