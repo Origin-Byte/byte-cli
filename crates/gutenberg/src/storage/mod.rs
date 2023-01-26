1e424aca25157718b10574fa0894e18af601f591
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-01-26 22:29:56 +0000

    Save state

--- crates/gutenberg/src/storage/mod.rs
+++ crates/gutenberg/src/storage/mod.rs
@@ -1,20 +1,20 @@
 use serde::{Deserialize, Serialize};
 
 pub mod aws;
-// pub mod nft_storage;
-// pub mod pinata;
+pub mod nft_storage;
+pub mod pinata;
 pub mod uploader;
 
 pub use aws::*;
-// pub use nft_storage::*;
-// pub use pinata::*;
+pub use nft_storage::*;
+pub use pinata::*;
 pub use uploader::*;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub enum Storage {
     Aws(AWSConfig),
     Undefined,
-    // Pinata(PinataConfig),
+    Pinata(PinataConfig),
     // Bundlr(BundlrConfig),
     // NftStorage(NftStorageConfig),
     // Shdw(ShdwConfig),
