93cdcf2b233775bdc0efcec1a9ec91aa8f6db66c
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-02-21 11:53:52 -0800

    Updated commit hashes

--- crates/byte_cli/src/consts.rs
+++ crates/byte_cli/src/consts.rs
@@ -7,6 +7,6 @@ pub const DEFAULT_CONFIG_FILENAME: &str = "config";
 pub const ROCKET_EMOJI: Emoji<'_, '_> = Emoji("🚀 ", "");
 
 // Package commits
-pub const SUI_PACKAGE_COMMIT: &str = "0ef3625";
-pub const ORIGINMATE_PACKAGE_COMMIT: &str = "99c0e38";
-pub const PROTOCOL_PACKAGE_COMMIT: &str = "ebf3e4f";
+pub const SUI_PACKAGE_COMMIT: &str = "83de679c";
+pub const ORIGINMATE_PACKAGE_COMMIT: &str = "4796885";
+pub const PROTOCOL_PACKAGE_COMMIT: &str = "81b87c37dfb6eef6051ee3e3c1c0a7fcd3eb29dc";
