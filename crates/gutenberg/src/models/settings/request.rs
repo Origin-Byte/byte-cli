d45ef251928d75d266542a5c1b26775baa546c95
Tamas Szucs <tamas@originbyte.io>
2023-05-08 15:27:47 +0200

    simplify request

--- crates/gutenberg/src/models/settings/request.rs
+++ crates/gutenberg/src/models/settings/request.rs
@@ -1,9 +1,4 @@
-use std::collections::HashSet;
-
-use bevy_reflect::{Reflect, Struct};
 use serde::{Deserialize, Serialize};
-
-use crate::err::GutenError;
 
 pub enum RequestType {
     Transfer,
@@ -29,63 +24,12 @@ impl Default for RequestPolicies {
 }
 
 impl RequestPolicies {
-    pub fn new(
-        transfer: bool,
-        borrow: bool,
-    ) -> RequestPolicies {
+    pub fn new(transfer: bool, borrow: bool) -> RequestPolicies {
         RequestPolicies { transfer, borrow }
     }
 
     pub fn is_empty(&self) -> bool {
         !self.transfer && !self.borrow
-    }
-
-    fn from_map(
-        map: &Vec<(String, bool)>,
-    ) -> Result<RequestPolicies, GutenError> {
-        let mut field_struct = RequestPolicies::default();
-
-        for (f, v) in map {
-            match f.as_str() {
-                "transfer" => {
-                    field_struct.transfer = *v;
-                    Ok(())
-                }
-                "borrow" => {
-                    field_struct.borrow = *v;
-                    Ok(())
-                }
-                other => Err(GutenError::UnsupportedSettings(format!(
-                    "The Request policy provided `{}` is not supported",
-                    other
-                ))),
-            }?;
-        }
-
-        Ok(field_struct)
-    }
-
-    pub fn fields() -> Vec<String> {
-        let field_struct = RequestPolicies::default();
-        let mut fields: Vec<String> = Vec::new();
-
-        for (i, _) in field_struct.iter_fields().enumerate() {
-            let field_name = field_struct.name_at(i).unwrap();
-
-            fields.push(field_name.to_string());
-        }
-        fields
-    }
-
-    pub fn to_map(&self) -> Vec<(String, bool)> {
-        let mut map: Vec<(String, bool)> = Vec::new();
-
-        for (i, value) in self.iter_fields().enumerate() {
-            let field_name = self.name_at(i).unwrap();
-            let value_ = value.downcast_ref::<bool>().unwrap();
-            map.push((field_name.to_string(), *value_));
-        }
-        map
     }
 
     pub fn write_policies(&self, type_name: &String) -> String {
