046f2003f33c56995171ce0b0a599cb714e55614
Nuno Boavida <45330362+nmboavida@users.noreply.github.com>
2023-07-19 17:29:23 +0100

    Update Sui SDK and API

--- crates/byte-api/src/responders/build_publish_tx.rs
+++ crates/byte-api/src/responders/build_publish_tx.rs
@@ -1,9 +1,16 @@
 use actix_web::{post, web, HttpResponse, Responder};
 use dotenv::dotenv;
 use rust_sdk::{coin::select_biggest_coin, publish, utils::get_context};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::{ffi::OsString, path::Path};
 use sui_sdk::types::{base_types::SuiAddress, transaction::TransactionData};
+
+#[derive(Deserialize)]
+pub struct RequestData {
+    sender: SuiAddress,
+    gas_budget: u64,
+    contract_dir: String,
+}
 
 #[utoipa::path(
     responses(
@@ -13,48 +20,20 @@ use sui_sdk::types::{base_types::SuiAddress, transaction::TransactionData};
     ),
 )]
 #[post("/build-publish-tx")]
-pub async fn build_publish_tx(
-    sender: web::Bytes,
-    gas_budget: web::Bytes,
-    contract_dir: web::Path<String>,
-) -> impl Responder {
+pub async fn build_publish_tx(data: web::Json<RequestData>) -> impl Responder {
     dotenv().ok();
-    let sender = match SuiAddress::from_bytes(sender.as_ref()) {
-        Ok(sender) => sender,
-        Err(_) => {
-            return HttpResponse::BadRequest().body("Invalid 'sender' address");
-        }
-    };
 
-    let gas_budget = match std::str::from_utf8(gas_budget.as_ref()) {
-        Ok(gas_budget) => gas_budget,
-        Err(_) => {
-            return HttpResponse::BadRequest()
-                .body("Invalid UTF-8 argument: 'gas_budget'");
-        }
-    };
-
-    let gas_budget = match gas_budget.parse::<u64>() {
-        Ok(gas_budget) => gas_budget,
-        Err(_) => {
-            return HttpResponse::BadRequest()
-                .body("Unable to pares gas_budget into u64 ");
-        }
-    };
-
-    let contract_dir = OsString::from(contract_dir.as_ref());
-    // Convert the `OsString` into a `Path`
-    let contract_dir = Path::new(&contract_dir);
+    let contract_dir = Path::new(&data.contract_dir);
 
     let wallet_ctx = get_context().await.unwrap();
     let client = wallet_ctx.get_client().await.unwrap();
-    let gas_coin = select_biggest_coin(&client, sender).await.unwrap();
+    let gas_coin = select_biggest_coin(&client, data.sender).await.unwrap();
 
     let tx_data_res = publish::prepare_publish_contract(
-        sender,
+        data.sender,
         contract_dir,
         (gas_coin.coin_object_id, gas_coin.version, gas_coin.digest),
-        gas_budget,
+        data.gas_budget,
     )
     .await;
 
